8e1d38be8feb8543fd5ee4971840ab3fb7af40d5
mattchrlw <mattchrlw@gmail.com>
2021-09-11 10:44:11 +1000

    Mock dataset and train menus

--- client/src/ui/Train.tsx
+++ client/src/ui/Train.tsx
@@ -1,62 +1,20 @@
 import React, { useState } from "react";
-import { Container, Dropdown, Form, Grid, GridColumn, Header, Segment } from "semantic-ui-react";
-
-const MOCK_DATASETS = [
-  {
-    key: "1",
-    value: "Sample Dataset 1",
-    text: "Sample Dataset 1",
-  },
-  {
-    key: "2",
-    value: "Sample Dataset 2",
-    text: "Sample Dataset 2",
-  },
-  {
-    key: "3",
-    value: "Sample Dataset 3",
-    text: "Sample Dataset 3",
-  },
-];
-
-const MOCK_LETTER_TO_SOUND = [
-  {
-    key: "1",
-    value: "Sample letter-to-sound file 1",
-    text: "Sample letter-to-sound file 1",
-  },
-  {
-    key: "2",
-    value: "Sample letter-to-sound file 2",
-    text: "Sample letter-to-sound file 2",
-  },
-  {
-    key: "3",
-    value: "Sample letter-to-sound file 3",
-    text: "Sample letter-to-sound file 3",
-  },
-];
-
-const MOCK_ENGINES = [
-  {
-    key: "1",
-    value: "Kaldi",
-    text: "Kaldi",
-  },
-  {
-    key: "2",
-    value: "ESPnet",
-    text: "ESPnet",
-  },
-  {
-    key: "3",
-    value: "HFT",
-    text: "HFT (beta)",
-  },
-];
+import {
+  Container,
+  Dropdown,
+  Form,
+  Grid,
+  GridColumn,
+  Header,
+  Segment,
+  Table,
+} from "semantic-ui-react";
+import { MOCK_DATASETS, MOCK_L2S, MOCK_ENGINES, MOCK_WORDLIST } from "../mock";
 
 export const Train: () => JSX.Element = () => {
   const [engine, setEngine] = useState(MOCK_ENGINES[0].value);
+  const [dataset, setDataset] = useState(MOCK_DATASETS[0].value);
+  const [l2s, setL2S] = useState(MOCK_L2S[0].value);
 
   return (
     <Container style={{ padding: "7em 0em 3em 0em" }}>
@@ -66,38 +24,65 @@ export const Train: () => JSX.Element = () => {
         pronunciation dictionary with letter-to-sound files, and selecting
         various model settings.
       </Segment>
-      <Form>
-      <Form.Field>
-      <label>Engine</label>
-        <Dropdown
-          placeholder="Engine"
-          fluid
-          selection
-          options={MOCK_ENGINES}
-          onChange={(_, { value }) => setEngine(value as string)}
-        />
-        </Form.Field>
-        <Form.Field>
-        <label>Dataset</label>
-        <Dropdown
-          placeholder="Dataset"
-          fluid
-          selection
-          options={MOCK_DATASETS}
-        />
-        </Form.Field>
-      {engine === "Kaldi" && (
-          <Form.Field>
-          <label>Letter-to-sound file</label>
-          <Dropdown
-            placeholder="Letter-to-sound file"
-            fluid
-            selection
-            options={MOCK_LETTER_TO_SOUND}
-          />
-          </Form.Field>
-      )}
-      </Form>
+      <Grid>
+        <Grid.Column width={8}>
+          <Form>
+            <Form.Field>
+              <label>Engine</label>
+              <Dropdown
+                placeholder="Engine"
+                fluid
+                selection
+                options={MOCK_ENGINES}
+                onChange={(_, { value }) => setEngine(value as string)}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Dataset</label>
+              <Dropdown
+                placeholder="Dataset"
+                fluid
+                selection
+                options={MOCK_DATASETS}
+                onChange={(_, { value }) => setDataset(value as string)}
+              />
+            </Form.Field>
+
+            {engine === "Kaldi" && (
+              <Form.Field>
+                <label>Letter-to-sound file</label>
+                <Dropdown
+                  placeholder="Letter-to-sound file"
+                  fluid
+                  selection
+                  options={MOCK_L2S}
+                  onChange={(_, { value }) => setL2S(value as string)}
+                />
+              </Form.Field>
+            )}
+          </Form>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          {engine === "Kaldi" && (
+            <Header as="h3">Letter-to-sound mappings</Header>
+          )}
+          <Container>
+            <Header as="h3">Word list</Header>
+            <Table>
+              <Table.Header>
+                <Table.HeaderCell>Word</Table.HeaderCell>
+                <Table.HeaderCell>Count</Table.HeaderCell>
+              </Table.Header>
+              {Array.from(MOCK_WORDLIST).map(([k, v]) => (
+                <Table.Row key={k}>
+                  <Table.Cell>{k}</Table.Cell>
+                  <Table.Cell>{v}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table>
+          </Container>
+        </Grid.Column>
+      </Grid>
     </Container>
   );
 };

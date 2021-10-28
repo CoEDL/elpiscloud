46e10806350115caf170886b7cf172d873b01c3c
nicklambourne <ndl93@live.com>
2021-10-29 01:03:47 +1100

    .

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,14 +4,16 @@ set -Eeuxo pipefail
 
 echo 'Running development environment setup for Elpis Cloud'
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == darwin* ]]; then
   if ! [ -x "$(command -v brew)" ]; then
     echo 'brew not found, installing now' >&2
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
-  echo 'Installing direnv'
-  brew install direnv
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  if ! [ -x "$(command -v direnv)" ]; then
+    echo 'Installing direnv'
+    brew install direnv
+  fi
+elif [[ "$OSTYPE" == linux-gnu* ]]; then
   if ! [ -x "$(command -v direnv)" ]; then
     echo 'direnv not found, installing now' >&2
     apt-get install -y direnv
@@ -21,10 +23,10 @@ else
   exit 2
 fi
 
-if [[ "$SHELL" == "*zsh" ]]; then
+if [[ "$SHELL" == *zsh ]]; then
   # shellcheck disable=SC2016
   echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc.local
-elif [[ "$SHELL" == "*bash" ]]; then
+elif [[ "$SHELL" == *bash ]]; then
   # shellcheck disable=SC2016
   echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 else
@@ -39,4 +41,4 @@ fi
 
 $SHELL
 
-echo 'Setup complete, restart your shell/terminal or change out of and back in to this directory to trigger direnv.'
+echo 'Development environment setup complete'

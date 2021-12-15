67698e020688b2eea2d1e3ebb14e4845fd2a9277
Harry Keightley <harrykeightley@outlook.com>
2021-12-15 12:50:09 +1000

    .

--- architecture/modules/functions/main.tf
+++ architecture/modules/functions/main.tf
@@ -14,20 +14,21 @@ resource "google_cloudfunctions_function" "function" {
 
 # A dedicated Cloud Storage bucket to store the zip source
 resource "google_storage_bucket" "source" {
-  name = "${var.project}-functions-source"
+  name     = "${var.project}-functions-source"
+  location = var.location
 }
 
 # Create a fresh archive of the current function folder
 data "archive_file" "functions" {
   type        = "zip"
   output_path = "temp/function_code_${timestamp()}.zip"
-  source_dir  = var.function_folder
+  source_dir  = var.functions_folder
 }
 
 # The archive in Cloud Stoage uses the md5 of the zip file
 # This ensures the Function is redeployed only when the source is changed.
 resource "google_storage_bucket_object" "archive" {
-  name = "${var.function_folder}_${data.archive_file.functions.output_md5}.zip" # will delete old items
+  name = "${var.functions_folder}_${data.archive_file.functions.output_md5}.zip" # will delete old items
 
   bucket = google_storage_bucket.source.name
   source = data.archive_file.functions_folder.output_path
--- architecture/modules/functions/variables.tf
+++ architecture/modules/functions/variables.tf
@@ -2,6 +2,7 @@ variable "region" {
     type = string
     default = "us-central1"
 }
+variable "location" {}
 variable "project" {}
 variable "functions_folder" {}
 variable "elpis_worker" {}

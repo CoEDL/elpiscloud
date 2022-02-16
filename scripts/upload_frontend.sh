f2d29a7e79d7bdbaecff6c7513585399fade6a44
Harry Keightley <harrykeightley@outlook.com>
2022-02-16 14:15:37 +1000

    Update frontend upload script

--- scripts/upload_frontend.sh
+++ scripts/upload_frontend.sh
@@ -4,7 +4,7 @@ env="$2"
 
 # Build the frontend
 cd client
-yarn install && yarn export
+yarn install && yarn build
 
 # Upload static files
 gsutil -m rsync -r -c -d "out" "gs://$project-$env-site"

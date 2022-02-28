6058eae0c67ff5c622cc84540dd8ce2c6c7eeffa
Harry Keightley <harrykeightley@outlook.com>
2022-02-28 18:47:54 +1000

    .

--- api/functions/utils.py
+++ api/functions/utils.py
@@ -4,7 +4,7 @@ from base64 import b64decode
 
 
 def decode_auth_header(user_info_header: str):
-    message_bytes = b64decode(user_info_header)
+    message_bytes = b64decode(user_info_header + '==')
     return message_bytes.decode('ascii')
 
 

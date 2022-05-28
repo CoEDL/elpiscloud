0d6d3f4d492855913305a5f43b21388533cb8544
Aviral Jain <aviraljain99@gmail.com>
2022-05-29 09:05:39 +1000

    Trying to fix tf errors

--- api/functions/main.py
+++ api/functions/main.py
@@ -1,12 +1,11 @@
 # Health Check function
-from api.functions.delete_datasets import delete_dataset_from_bucket
 from health_check import hello
 
 # File uploads
 from sign_files import sign_files
 from storage_watcher import storage_watcher
 
-# Dataset Uploads
+# Dataset Uploads and Deletes
 from process_dataset import process_dataset
 from process_file import process_dataset_file
 from delete_datasets import delete_dataset_from_bucket

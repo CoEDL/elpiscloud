2be76beab7d681fff8f09e878ee34dc701d9d6c6
Harry Keightley <harrykeightley@outlook.com>
2022-09-20 16:59:25 +1000

    Added tests for generating annotations from elan files

--- functions/tests/test_annotations.py
+++ functions/tests/test_annotations.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+from pprint import pprint
+
 from pytest import raises
-from utils.annotation import Annotation
+from utils.annotation import (
+    Annotation,
+    get_annotations_by_tier_name,
+    get_annotations_by_tier_order,
+    get_annotations_by_tier_type,
+)
 
 VALID_ANNOTATION_DATA = {"audio_file_name": "audio", "transcript": "hi there"}
 INVALID_ANNOTATION_DATA = {"audio_file_name": "audio"}
+
+DATA_DIR = Path(__file__).parent / "data"
+ELAN_PATH = DATA_DIR / "test.eaf"
 
 
 def test_annotation_from_valid_dict():
@@ -14,3 +25,30 @@ def test_annotation_from_valid_dict():
 def test_annotation_from_invalid_dict():
     with raises(Exception):
         Annotation.from_dict(INVALID_ANNOTATION_DATA)
+
+
+def test_generate_utterances_from_tier_id():
+    annotations = get_annotations_by_tier_name(ELAN_PATH, "Phrase")
+    assert len(annotations) == 2
+    assert all(annotation.speaker_id == "SL" for annotation in annotations)
+
+
+def test_missing_linguistic_type():
+    annotations = get_annotations_by_tier_type(ELAN_PATH, "missing")
+    assert len(annotations) == 0
+
+
+def test_generate_utterances_from_linguistic_type():
+    annotations = get_annotations_by_tier_type(ELAN_PATH, "default-lt")
+    assert len(annotations) == 6
+
+
+def test_invalid_tier_order():
+    annotations = get_annotations_by_tier_order(ELAN_PATH, 69)
+    assert len(annotations) == 0
+
+
+def test_generate_utterances_from_tier_order():
+    for tier_order in range(1, 4):
+        annotations = get_annotations_by_tier_order(ELAN_PATH, tier_order)
+        assert len(annotations) == 2

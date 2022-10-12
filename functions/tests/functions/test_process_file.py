5fa12c5acdf413b5d513ae65f831cc4a0d375483
Harry Keightley <harrykeightley@outlook.com>
2022-10-12 14:43:11 +1000

    Fix invalid checking for being finished processing

--- functions/tests/functions/test_process_file.py
+++ functions/tests/functions/test_process_file.py
@@ -30,6 +30,9 @@ TEST_JOB = ProcessingJob(
 )
 
 
+ABUI_DATASET_FILES = ["abui_1.eaf", "abui_1.wav", "abui_2.eaf", "abui_2.wav"]
+
+
 def test_download_files(mocker):
     downloader_mock: Mock = mocker.patch(
         "functions.datasets.process_file.download_blob"
@@ -83,27 +86,32 @@ def test_generate_training_files_with_timed_annotation(tmp_path: Path, mocker):
 
 
 def test_has_finished_processing():
-    dataset_files = ["1.eaf", "1.wav", "2.eaf", "2.wav"]
-    processed_files = ["1.json", "1.wav"]
-    assert not has_finished_processing(dataset_files, processed_files)
+    processed_files = ["abui_1.json", "abui_1.wav", "abui_2.json", "abui_2.wav"]
+    assert has_finished_processing(ABUI_DATASET_FILES, processed_files)
 
-    processed_files = ["1.json", "1.wav", "2.json", "2.wav"]
-    assert has_finished_processing(dataset_files, processed_files)
 
+def test_has_finished_processing_with_incomplete_files_should_return_false():
+    processed_files = ["abui_1.json", "abui_1.wav"]
+    assert not has_finished_processing(ABUI_DATASET_FILES, processed_files)
+
+
+def test_has_finished_processing_with_timed_annotations():
     processed_files = [
-        "1.json",
-        "1.wav",
-        "2_1000.json",
-        "2_1000.wav",
+        "abui_1.json",
+        "abui_1.wav",
+        "abui_2_1000.json",
+        "abui_2_1000.wav",
     ]
-    assert has_finished_processing(dataset_files, processed_files)
+    assert has_finished_processing(ABUI_DATASET_FILES, processed_files)
 
+
+def test_has_finished_processing_with_timed_and_multiple_annotations():
     processed_files = [
-        "1.json",
-        "1.wav",
-        "2_1000.json",
-        "2_1000.wav",
-        "2_3000.json",
-        "2_3000.wav",
+        "abui_1.json",
+        "abui_1.wav",
+        "abui_2_1000.json",
+        "abui_2_1000.wav",
+        "abui_2_3000.json",
+        "abui_2_3000.wav",
     ]
-    assert has_finished_processing(dataset_files, processed_files)
+    assert has_finished_processing(ABUI_DATASET_FILES, processed_files)
